9e923988d075a8bcaa390f0c78fe5c252d0f3979
zzq <16670157859@163.com>
2025-07-09 15:53:34 +0800

    feat:完成订单逻辑

---
+++ order.js
@@ -0,0 +1,12 @@
+console.log("gggg");
+console.log("order1");
+console.log("order1");
+console.log("order1");
+
+for (let i = 0; i < 10; i++) {
+  console.log(`order${i + 1}`);
+}
+
+console.log("order1");
+console.log("order1");
+console.log("fgsf");
